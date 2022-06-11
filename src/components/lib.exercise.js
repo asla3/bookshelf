83a87e6e4e4b5d904e04fc59e1c71bce6c73bae6
Andrés Loreto <andresantigoloretoaguero@gmail.com>
2022-06-11 16:04:30 -0400

    Finished 2.2

--- src/components/lib.exercise.js
+++ src/components/lib.exercise.js
@@ -1,15 +1,25 @@
-import styled from '@emotion/styled'
+import styled from '@emotion/styled/macro'
 import {Dialog as ReachDialog} from '@reach/dialog'
 
-const Button = styled.button(({variant = 'primary'}) => ({
-  backgroundColor:
-    variant === 'primary' ? 'rgb(63, 81, 181)' : 'rgb(241, 242, 247)',
-  border: '0px',
-  lineHeight: 1,
-  color: variant === 'primary' ? 'white' : 'rgb(67, 68, 73)',
-  borderRadius: '3px',
-  padding: '10px 15px',
-}))
+const buttonVariants = {
+  primary: {
+    background: '#3f51b5',
+    color: 'white',
+  },
+  secondary: {
+    background: '#f1f2f7',
+    color: '#434449',
+  },
+}
+const Button = styled.button(
+  {
+    padding: '10px 15px',
+    border: '0',
+    lineHeight: '1',
+    borderRadius: '3px',
+  },
+  ({variant = 'primary'}) => buttonVariants[variant],
+)
 
 const Input = styled.input({
   borderRadius: '3px',
@@ -17,8 +27,6 @@ const Input = styled.input({
   background: '#f1f2f7',
   padding: '8px 12px',
 })
-
-const FormGroup = styled.div({display: 'flex', flexDirection: 'column'})
 
 const CircleButton = styled.button({
   borderRadius: '30px',
@@ -47,4 +55,9 @@ const Dialog = styled(ReachDialog)({
   },
 })
 
-export {CircleButton, Dialog, Button, Input, FormGroup}
+const FormGroup = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+})
+
+export {Button, Input, CircleButton, Dialog, FormGroup}
